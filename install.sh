9139c0450cff1678e3c49aba35fd63bcf914b4be
Athlon1600 <Athlon2400@gmail.com>
2021-01-05 16:59:30 -0600

    added npm forever option

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get -y install nginx
+
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+sudo cp -r node_proxy.conf /etc/nginx/sites-available/default
+##sudo ln -s /etc/nginx/sites-available/laravel.conf /etc/nginx/sites-enabled/
